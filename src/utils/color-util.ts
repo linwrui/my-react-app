8c514ab65bc518ae29f1ab86ce394f8426345e60
linwrui <linwrui@163.com>
2021-03-21 14:33:28 +0800

    调整 getReverseForegroundColor

--- src/utils/color-util.ts
+++ src/utils/color-util.ts
@@ -8,5 +8,5 @@
  */
 export function getReverseForegroundColor(color: number[]) {
   const grayLevel = (0.299 * color[0] + 0.587 * color[1] + 0.114 * color[2]) / 255;
-  return grayLevel > 0.5 ? '#333' : '#D2D2D2';
+  return grayLevel > 0.5 ? '#333' : '#DDDDDD';
 }
