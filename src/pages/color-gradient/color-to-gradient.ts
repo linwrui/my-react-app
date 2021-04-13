72d6c0b874d90705f4f30da6ca321a92b428aaee
linwrui <wenrui.lin@brilliant-solution.com>
2021-04-13 14:59:43 +0800

    优化渐变色DEMO的算法

--- src/pages/color-gradient/color-to-gradient.ts
+++ src/pages/color-gradient/color-to-gradient.ts
@@ -1,31 +1,66 @@
-import rgba from 'color-rgba';
+import { hsl } from 'd3-color';
 
 export interface GradientTransformParams {
-    angle?: number;
-    lighting?: number; // from 0 to 255. The larger the number is the brighter second color
+  angle?: number;
+  lighting?: number; // from 0 to 255. The larger the number is the brighter second color
+  colorStops?: Array<{
+    h?: number | string;
+    s?: number | string;
+    l?: number | string;
+    opacity?: number | string;
+  }>;
 }
 
 export const defaultTransformParams: GradientTransformParams = {
-    angle: 135,
-    lighting: 255
+  angle: 135,
+  lighting: 255,
 };
 
-export function colorToGradient(basicColor: string, transformParams?: GradientTransformParams) {
+export function colorToLinearGradient(basicColor: string, transformParams?: GradientTransformParams) {
   const params = Object.assign(defaultTransformParams, transformParams || {});
-  const rgbaArr = rgba(basicColor);
-  if (rgbaArr != null) {
-    const red = rgbaArr[0];
-    const green = rgbaArr[1];
-    const blue = rgbaArr[2];
-    const alpha = rgbaArr[3];
+  const color = hsl(basicColor);
+  if (color != null) {
+    const transform = (source: number, to: number | string | undefined) => {
+      if (typeof to === 'string') {
+        if (to.startsWith('+')) {
+          return source + Number(to.match(/\+(.*)/)?.[1]);
+        }
+        if (to.startsWith('-')) {
+          return source - Number(to.match(/-(.*)/)?.[1]);
+        }
+      } else if (typeof to === 'number') {
+        return to;
+      }
+      return source;
+    };
+    let colorStops: string[] | undefined = transformParams?.colorStops
+      ?.map(x =>
+        hsl(
+          transform(color.h, x.h),
+          transform(color.s, x.s),
+          transform(color.l, x.l),
+          transform(color.opacity, x.opacity)
+        )
+      )
+      .map(x => x.toString());
+    if (colorStops == null) {
+      const red = color.rgb().r;
+      const green = color.rgb().g;
+      const blue = color.rgb().b;
+      const alpha = color.opacity;
 
-    let secondColor = "rgba(0, 0, 0, 0)";
-    const lighting = Number(params.lighting); 
+      let secondColor = 'rgba(0, 0, 0, 0)';
+      const lighting = Number(params.lighting);
 
-    if (red > green && red > blue) secondColor = `rgba(${red}, ${Math.abs(green - lighting)}, ${Math.abs(blue - lighting)}, ${alpha})`;
-    else if (green > red && green > blue) secondColor = `rgba(${Math.abs(red - lighting)}, ${green}, ${Math.abs(blue - lighting)}, ${alpha})`;
-    else if (blue > red && blue > green) secondColor = `rgba(${Math.abs(red - lighting)}, ${Math.abs(green - lighting)}, ${blue}, ${alpha})`;
-    return `linear-gradient(${params.angle}deg, ${basicColor}, ${secondColor})`;
+      if (red > green && red > blue)
+        secondColor = `rgba(${red}, ${Math.abs(green - lighting)}, ${Math.abs(blue - lighting)}, ${alpha})`;
+      else if (green > red && green > blue)
+        secondColor = `rgba(${Math.abs(red - lighting)}, ${green}, ${Math.abs(blue - lighting)}, ${alpha})`;
+      else if (blue > red && blue > green)
+        secondColor = `rgba(${Math.abs(red - lighting)}, ${Math.abs(green - lighting)}, ${blue}, ${alpha})`;
+      colorStops = [basicColor, secondColor];
+    }
+    return `linear-gradient(${params.angle}deg, ${colorStops?.join(', ')})`;
   }
   return basicColor;
 }
--- src/pages/color-gradient/index.tsx
+++ src/pages/color-gradient/index.tsx
@@ -2,10 +2,12 @@ import { Divider, Form, InputNumber } from 'antd';
 import rgba from 'color-rgba';
 import React from 'react';
 import { RgbaColorPicker } from 'react-colorful';
-import { colorToGradient, defaultTransformParams, GradientTransformParams } from './color-to-gradient';
+import { colorToLinearGradient, defaultTransformParams, GradientTransformParams } from './color-to-gradient';
+import './style.less';
 
 interface ColorGradientState {
   color: string;
+  blur: number;
   transformParams: GradientTransformParams;
 }
 
@@ -14,11 +16,12 @@ export class ColorGradient extends React.Component<unknown, ColorGradientState> 
     super(props);
     this.state = {
       color: 'rgba(255, 0, 0, 1)',
+      blur: 3,
       transformParams: Object.assign(defaultTransformParams, {}),
     };
   }
   render() {
-    const { color, transformParams } = this.state;
+    const { color, transformParams, blur } = this.state;
     const rgbaArr = rgba(color) as number[];
     return (
       <div style={{ padding: 10 }} className="color-gradient">
@@ -31,37 +34,39 @@ export class ColorGradient extends React.Component<unknown, ColorGradientState> 
               color={{ r: rgbaArr[0], g: rgbaArr[1], b: rgbaArr[2], a: rgbaArr[3] }}
               onChange={c => this.setState({ color: `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})` })}
             />
+            <Form style={{ padding: 10 }} layout="inline">
+              <Form.Item name="angle" label="angle">
+                <InputNumber
+                  defaultValue={transformParams.angle}
+                  min={-180}
+                  max={180}
+                  onChange={e => this.setState({ transformParams: Object.assign(transformParams, { angle: e }) })}
+                />
+              </Form.Item>
+              <Form.Item
+                name="blur"
+                label="blur">
+                <InputNumber
+                  defaultValue={blur}
+                  min={1}
+                  onChange={e => this.setState({ blur: e as number })}
+                />
+              </Form.Item>
+            </Form>
           </div>
         </div>
         <Divider />
         <div className="gradient-preview">
           <div style={{ padding: 10 }}>
-            gradient preview: <span>{colorToGradient(color, transformParams)}</span>
+            gradient preview: <br/> <span>{colorToLinearGradient(color, transformParams)}</span>
           </div>
-          <Form style={{padding: 10}} layout="inline">
-            <Form.Item name="angle" label="angle">
-              <InputNumber
-                defaultValue={transformParams.angle}
-                min={-180}
-                max={180}
-                onChange={(e) => this.setState({ transformParams: Object.assign(transformParams, {angle: e}) })}
-              />
-            </Form.Item>
-            <Form.Item validateStatus="validating" help="from 0 to 255. The larger the number is the brighter second color" name="lighting" label="lighting">
-              <InputNumber
-                defaultValue={transformParams.lighting}
-                min={1}
-                max={255}
-                onChange={(e) => this.setState({ transformParams: Object.assign(transformParams, {lighting: e}) })}
-              />
-            </Form.Item>
-          </Form>
           <div
             style={{
-              border: '1px solid #BFBFBF',
-              width: 200,
-              height: 150,
-              background: colorToGradient(color, transformParams),
+              backdropFilter: `blur(${blur}px)`,
+              background: colorToLinearGradient(color, {
+                ...transformParams,
+                colorStops: [{ opacity: 0.4 }, { opacity: 0.2 }, { opacity: 0.1 }],
+              }),
             }}
             className="gradient-grid"
           />
