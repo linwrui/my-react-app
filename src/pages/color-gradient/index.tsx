a1a4633d4b6512ec83843decd33fe0593e38ef20
linwrui <wenrui.lin@brilliant-solution.com>
2021-04-13 15:36:14 +0800

    添加border设置

--- src/pages/color-gradient/index.tsx
+++ src/pages/color-gradient/index.tsx
@@ -1,13 +1,15 @@
 import { Divider, Form, InputNumber } from 'antd';
-import rgba from 'color-rgba';
+import { rgb } from 'd3-color';
 import React from 'react';
 import { RgbaColorPicker } from 'react-colorful';
 import { colorToLinearGradient, defaultTransformParams, GradientTransformParams } from './color-to-gradient';
 import './style.less';
 
 interface ColorGradientState {
-  color: string;
-  blur: number;
+  backgroundColor: string;
+  backgroundblur: number;
+  borderColor: string;
+  borderWidth: number;
   transformParams: GradientTransformParams;
 }
 
@@ -15,50 +17,67 @@ export class ColorGradient extends React.Component<unknown, ColorGradientState> 
   constructor(props: unknown) {
     super(props);
     this.state = {
-      color: 'rgba(255, 0, 0, 1)',
-      blur: 3,
+      backgroundColor: 'rgba(255, 0, 0, 1)',
+      borderColor: '#2B56AB',
+      backgroundblur: 3,
+      borderWidth: 2,
       transformParams: Object.assign(defaultTransformParams, {}),
     };
   }
   render() {
-    const { color, transformParams, blur } = this.state;
-    const rgbaArr = rgba(color) as number[];
+    const { backgroundColor, transformParams, backgroundblur, borderColor, borderWidth } = this.state;
+    const backgroundRgb = rgb(backgroundColor);
+    const borderRgb = rgb(borderColor);
     return (
       <div style={{ padding: 10 }} className="color-gradient">
         <div className="basic-color">
-          <div style={{ padding: 10 }}>
-            color: <span>{color}</span>
-          </div>
           <div className="gradient-params">
-            <RgbaColorPicker
-              color={{ r: rgbaArr[0], g: rgbaArr[1], b: rgbaArr[2], a: rgbaArr[3] }}
-              onChange={c => this.setState({ color: `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})` })}
-            />
-            <Form style={{ padding: 10 }} layout="inline">
-              <Form.Item name="angle" label="angle">
-                <InputNumber
-                  defaultValue={transformParams.angle}
-                  min={-180}
-                  max={180}
-                  onChange={e => this.setState({ transformParams: Object.assign(transformParams, { angle: e }) })}
-                />
-              </Form.Item>
-              <Form.Item name="blur" label="blur">
-                <InputNumber defaultValue={blur} min={1} onChange={e => this.setState({ blur: e as number })} />
-              </Form.Item>
-            </Form>
+            <div style={{ padding: 10 }}>
+              background: <span>{backgroundColor}</span>
+              <RgbaColorPicker
+                color={{ r: backgroundRgb.r, g: backgroundRgb.g, b: backgroundRgb.b, a: backgroundRgb.opacity }}
+                onChange={c => this.setState({ backgroundColor: `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})` })}
+              />
+              <Form style={{ padding: 10 }} layout="inline">
+                <Form.Item name="angle" label="angle">
+                  <InputNumber
+                    defaultValue={transformParams.angle}
+                    min={-180}
+                    max={180}
+                    onChange={e => this.setState({ transformParams: Object.assign(transformParams, { angle: e }) })}
+                  />
+                </Form.Item>
+                <Form.Item name="blur" label="blur">
+                  <InputNumber defaultValue={backgroundblur} min={1} onChange={e => this.setState({ backgroundblur: e as number })} />
+                </Form.Item>
+              </Form>
+            </div>
+            <div style={{ padding: 10 }}>
+              border: <span>{borderColor}</span>
+              <RgbaColorPicker
+                color={{ r: borderRgb.r, g: borderRgb.g, b: borderRgb.b, a: borderRgb.opacity }}
+                onChange={c => this.setState({ borderColor: `rgba(${c.r}, ${c.g}, ${c.b}, ${c.a})` })}
+              />
+              <Form style={{ padding: 10 }} layout="inline">
+                <Form.Item name="borderWidth" label="borderWidth">
+                  <InputNumber defaultValue={borderWidth} min={1} onChange={e => this.setState({ borderWidth: e as number })} />
+                </Form.Item>
+              </Form>
+            </div>
           </div>
         </div>
         <Divider />
         <div className="gradient-preview">
           <div style={{ padding: 10 }}>
-            gradient preview: <br /> <span>{colorToLinearGradient(color, transformParams)}</span>
+            gradient preview: <br /> <span>{colorToLinearGradient(borderColor, transformParams)}</span>
           </div>
           <div
             style={{
-              backdropFilter: `blur(${blur}px)`,
-              border: '2px solid',
-              borderImage: `${colorToLinearGradient('#2B56AB', {
+              backdropFilter: `blur(${backgroundblur}px)`,
+              borderWidth,
+              borderStyle: `solid`,
+              borderRadius: 4,
+              borderImage: `${colorToLinearGradient(borderColor, {
                 ...transformParams,
                 colorStops: [
                   { opacity: 0.1 },
@@ -69,7 +88,7 @@ export class ColorGradient extends React.Component<unknown, ColorGradientState> 
                   { opacity: 0.1 },
                 ],
               })} 10`,
-              background: colorToLinearGradient(color, {
+              background: colorToLinearGradient(backgroundColor, {
                 ...transformParams,
                 colorStops: [{ opacity: 0.4 }, { opacity: 0.2, position: '25%' }, { opacity: 0.1 }],
               }),
